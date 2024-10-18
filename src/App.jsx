07a2ed6b721dcefe54fadf6605b207539960f1c4
Angel Trendafilov <aatrendafilov@gmail.com>
2024-10-18 17:52:16 +0300

    Fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 
+// BrowserRouter for SEO optimize
 // Lazy load your pages
 const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
